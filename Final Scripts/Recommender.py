64926459bc644e9a54ac2176898b78d921ae1516
avinash madasu <avinash.sai001@gmail.com>
2017-10-22 19:46:41 +0530

    Add files via upload

--- Final Scripts/Recommender.py
+++ Final Scripts/Recommender.py
@@ -1,4 +1,3 @@
-#!/bin/python
 from movielens import *
 import numpy as np
 from sklearn.cluster import KMeans
@@ -58,13 +57,10 @@ plt.plot(reduced_data[:,0],reduced_data[:,1],'k.',markersize=2)
 plt.scatter(centroids[:,0],centroids[:,1],marker='x', s=169, linewidths=3,
             color='w', zorder=10)
 plt.show()
-'''
-
 
 utility = np.zeros((n_users, n_items))
 for r in rating:
     utility[r.user_id-1][r.item_id-1] = r.rating
-
 # Finds the average rating for each user and stores it in the user's object
 for i in range(n_users):
     rated = np.nonzero(utility[i])
@@ -73,10 +69,8 @@ for i in range(n_users):
         user[i].avg_r = np.mean(utility[i][rated])
     else:
         user[i].avg_r = 0.
-
 #print utility
 clustered_matrix=[]
-
 for i in range(0,n_users):
     mean_matrix=[]
     for j in range(0,length):
@@ -92,10 +86,7 @@ for i in range(0,n_users):
              value[j]=np.mean(mean_matrix[j])
  
     clustered_matrix.append(value)
-
 clustered_matrix=np.array(clustered_matrix)
-
-
 for i in range(0,n_users):
     mean_matrix=clustered_matrix[i]
     count=0
@@ -108,7 +99,6 @@ for i in range(0,n_users):
         user[i].avg_r=(sum/count)
     else:
         user[i].avg_r=0.0
-
 # Finds the Pearson Correlation Similarity Measure between two users
 def pcs(x, y):
     x_r=0
@@ -131,8 +121,6 @@ def pcs(x, y):
         return (x_r*y_r)/(math.sqrt(x_r_2)*math.sqrt(y_r_2))
     else:
         return 0
-
-
 def normalization():
     normalize_rating=np.zeros((n_users,length))
     for i in range(0,n_users):
@@ -142,18 +130,12 @@ def normalization():
             else:
                 normalize_rating[i][j]=9999999
     return normalize_rating
-
 # Guesses the ratings that user with id, user_id, might give to item with id, i_id.
 # We will consider the top_n similar users to do this
-
 Pearson_matrix=np.zeros((n_users,n_users))
-
 for i in range(0,n_users):
     for j in range(0,n_users):
         Pearson_matrix[i][j]=pcs(i+1,j+1)
-
-
-
 #print(len(Pearson_matrix[0]))
 def guess(user_id, i_id, top_n):
     rating_similarity=[]
@@ -164,10 +146,7 @@ def guess(user_id, i_id, top_n):
         if(i!=user_id-1):
             rating_similarity.append(Pearson_matrix[user_id-1][i])
         
-
     rating_new= [x for (y,x) in sorted(zip(rating_similarity,temp), key=lambda pair: pair[0], reverse=True)]
-
-
     count=0
     sum=0
     for i in range(0,top_n):
@@ -179,42 +158,27 @@ def guess(user_id, i_id, top_n):
         cal_rating=user[user_id-1].avg_r
     else:
         cal_rating=(sum/count)+user[user_id-1].avg_r
-
     if(cal_rating<1.0 or cal_rating >5.0):
         return setlimit(cal_rating)
     else:
         return cal_rating
-
 final_ratings=np.array(clustered_matrix)
-
 for i in range(0,n_users):
     for j in range(0,length):
         if(final_ratings[i][j]==0):
             final_ratings[i][j]=guess(i+1,j+1,150)
-
-
-
-
 utility_test = np.zeros((n_users, n_items))
 for r in rating_test:
     utility_test[r.user_id-1][r.item_id-1] = r.rating
-
 #data=pd.read_csv("data/u.test",header=None,delimiter=r"\s+")
-
 y_pred=[]
-
 y_true=[]
-
-#f=open('predictions7.txt','w')
-
+f=open('predictions.txt','w')
 for i in range(0,n_users):
     for j in range(0,n_items):
         if(utility_test[i][j]>0):
             y_pred.append(final_ratings[i][kmeans.labels_[j]-1])
             y_true.append(utility_test[i][j])
-            #f.write("%d, %d, %.4f\n" % (i+1, j+1, final_ratings[i][kmeans.labels_[j]-1]))
-
-#f.close()
+            f.write("%d, %d, %.4f\n" % (i+1, j+1, final_ratings[i][kmeans.labels_[j]-1]))
+f.close()
 print(mean_squared_error(y_pred,y_true))
-
-'''
